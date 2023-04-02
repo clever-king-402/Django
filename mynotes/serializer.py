517dfdca6bf2fd1c2f785158412220943d14b611
clever-king-402 <122508802+clever-king-402@users.noreply.github.com>
2023-04-02 20:16:14 +0545

    Add files via upload

---
+++ mynotes/serializer.py
@@ -0,0 +1,6 @@
+from rest_framework.serializers import ModelSerializer
+from .models import *
+class NoteSerializer(ModelSerializer):
+    class Meta:
+        model = Note
+        fields = '__all__' #['body','updated'];
--- mynotes/urls.py
+++ mynotes/urls.py
@@ -1,22 +1,11 @@
-"""mynotes URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/",include("api.urls"))
+    path('',views.getRoutes,name="routes"),
+    path('notes/',views.getNotes,name="notes"),
+    path('notes/create/',views.createNote,name='create-note'),
+    path('notes/<str:pk>/update/',views.updateNote,name="update-note"),
+    path('notes/<str:pk>/delete/',views.deleteNote,name="delete-note"),
+    path('notes/<str:pk>/',views.getNote,name="note"),
 ]
